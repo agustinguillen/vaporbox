f208c15acaa5f6c6ecd3e0604e4a10adf637f296
Agustin Guillen <agustinnguillen@gmail.com>
2021-06-14 11:54:47 -0300

    Socket y Api configurados en mismo puerto

--- api/app.js
+++ api/app.js
@@ -2,7 +2,6 @@
 
 let express = require('express');
 let app = express();
-var server = require('http').createServer(app)
 let path = require('path');
 
 //cargar rutas
@@ -38,62 +37,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('*', (req, res)=>{
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
-
-//sockets
-const io = require("socket.io")(server, {
-    cors: {
-      origin: "*",
-      methods: ["GET", "POST"],
-      allowedHeaders: ["Authorization"],
-      credentials: true
-    }
-  });
-
-let users = [];
-
-const addUser = (userId, socketId)=>{
-  !users.some(user=>user.userId === userId) &&
-  users.push({ userId, socketId });
-}
-
-const removeUser = (socketId)=>{
-  users = users.filter(user => user.socketId !== socketId);
-}
-
-const getUser = (userId)=>{
-  return users.find((user)=> user.userId === userId);
-}
-
-let sentMessage;
-
-io.on('connection', (socket)=>{
-  socket.on("addUser", userId=>{
-    addUser(userId, socket.id);
-    io.emit("getUsers", users);
-  });
-
-  socket.on("sendMessage", (message)=>{
-    sentMessage = message;
-    let user = getUser(sentMessage.receiver);
-    if(user){
-      io.to(user.socketId).emit("getMessage", message);
-    }
-  });
-
-  socket.on("notificationPublication", (notification)=>{
-    let user = getUser(notification.notification.author);
-    if(user){
-      io.to(user.socketId).emit("newNotification", notification);
-    }
-  });
-
-  socket.on("disconnect", ()=>{
-    removeUser(socket.id);
-    io.emit("getUsers", users);
-  })
-});
-
-server.listen(80);
 
 //exportar
 module.exports = app;
--- api/index.js
+++ api/index.js
@@ -4,6 +4,60 @@ let port = process.env.PORT || 80;
 let mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 
+//Sockets
+const io = require("socket.io")(app, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+      allowedHeaders: ["Authorization"],
+      credentials: true
+    }
+  });
+
+let users = [];
+
+const addUser = (userId, socketId)=>{
+  !users.some(user=>user.userId === userId) &&
+  users.push({ userId, socketId });
+}
+
+const removeUser = (socketId)=>{
+  users = users.filter(user => user.socketId !== socketId);
+}
+
+const getUser = (userId)=>{
+  return users.find((user)=> user.userId === userId);
+}
+
+let sentMessage;
+
+io.on('connection', (socket)=>{
+  socket.on("addUser", userId=>{
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  socket.on("sendMessage", (message)=>{
+    sentMessage = message;
+    let user = getUser(sentMessage.receiver);
+    if(user){
+      io.to(user.socketId).emit("getMessage", message);
+    }
+  });
+
+  socket.on("notificationPublication", (notification)=>{
+    let user = getUser(notification.notification.author);
+    if(user){
+      io.to(user.socketId).emit("newNotification", notification);
+    }
+  });
+
+  socket.on("disconnect", ()=>{
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  })
+});
+
 //Conexión DB
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
