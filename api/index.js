675761645b4fb8fea7d49e1a473359e5da330fec
Agustin Guillen <agustinnguillen@gmail.com>
2021-06-15 17:26:43 -0300

    Modificación de estructura de sockets en backend

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 'use strict'
 let app = require('./app');
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 let port = process.env.PORT || 8080;
 let mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
 
 //Conexión DB
 mongoose.set('useFindAndModify', false);
@@ -11,10 +12,62 @@ mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopo
         .then(()=>{
             console.log("La conexión a la base de datos vaporboxdb se realizó con éxito")
 
-            //Crear servidor
-            app.listen(port, ()=>{
-                console.log("Servidor corriendo en puerto 8080")
+            //SOCKETS
+            
+            const server = require('http').Server(app);
+            const io = require('socket.io')(server);
+            server.listen(port, () => {
+                console.log(`Server started: PORT`)
+            })
+            
+            let users = [];
+            
+            const addUser = (userId, socketId)=>{
+                !users.some(user=>user.userId === userId) &&
+                users.push({ userId, socketId });
+            }
+                
+            const removeUser = (socketId)=>{
+                users = users.filter(user => user.socketId !== socketId);
+            }
+            
+            const getUser = (userId)=>{
+                return users.find((user)=> user.userId === userId);
+            }
+            
+            let sentMessage;
+            
+            io.on("connection", (socket)=>{
+                socket.on("addUser", userId=>{
+                    addUser(userId, socket.id);
+                    io.emit("getUsers", users);
+                    console.log("alguien se conectooo")
+                });
+                
+                socket.on("sendMessage", (message)=>{
+                    sentMessage = message;
+                    let user = getUser(sentMessage.receiver);
+                    if(user){
+                    io.to(user.socketId).emit("getMessage", message);
+                }
             });
+            
+            socket.on("notificationPublication", (notification)=>{
+                let user = getUser(notification.notification.author);
+                    if(user){
+                        io.to(user.socketId).emit("newNotification", notification);
+                    }
+                })
+                
+                socket.on("disconnect", ()=>{
+                    removeUser(socket.id);
+                    io.emit("getUsers", users);
+                })
+            });
+
         })
         .catch(err => console.log(err));
-
+        
+        
+        
+        
