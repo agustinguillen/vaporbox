d6b3775d71da227e577077a2c97c39216b4a254d
agustinguillen <agustinnguillen@gmail.com>
2023-06-06 13:12:29 -0300

    fix url

--- api/public/main.js
+++ api/public/main.js
@@ -1163,7 +1163,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 const environment = {
     production: false,
-    url: 'http://localhost:8080/api/'
+    url: 'https://vaporbox.onrender.com/'
 };
 /*
  * For easier debugging in development mode, you can import the following file
